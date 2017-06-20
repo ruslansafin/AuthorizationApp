84a148eb4f3266df2e3e8c5726119a3b78a4d9ea
Ruslan Safin <ruslan.safin1@yandex.ru>
2017-06-20 18:49:10 +0300

    Starting some activity by clicking on the corresponding buttons

--- app/src/main/java/com/example/user/authorizationapp/MainActivity.java
+++ app/src/main/java/com/example/user/authorizationapp/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.user.authorizationapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends /*AppCompat*/Activity {
 
     private Button okButton;
+    private Button regButton;
     private Context context;
 
     @Override
@@ -17,13 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this;
-        okButton = (Button) findViewById(R.id.okButton);
-        okButton.setOnClickListener(new View.OnClickListener(){
 
+        okButton = (Button) findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(context, HelloActivity.class));
             }
         });
+
+        regButton = (Button) findViewById(R.id.regButton);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, RegActivity.class));
+            }
+        });
     }
+
 }
